5b1b141ebb61a93ed9abc4bbb6b8dcc7a138deaa
limjintaek <jintaek019@gmail.com>
2025-04-25 17:13:29 +0900

    프로젝트 생성

---
+++ src/test/java/com/baedal/product/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baedal.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
