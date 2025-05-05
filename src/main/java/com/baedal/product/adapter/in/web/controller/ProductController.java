99ebae6db4bb32699bea9ab3cf78ab0562cbd84f
limjintaek <jintaek019@gmail.com>
2025-05-05 21:40:16 +0900

    [#4] fix. 매핑 주소 오표기 수정

--- src/main/java/com/baedal/product/adapter/in/web/controller/ProductController.java
+++ src/main/java/com/baedal/product/adapter/in/web/controller/ProductController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/ap/product/v0")
+@RequestMapping("/api/product/v0")
 @RequiredArgsConstructor
 public class ProductController {
 
